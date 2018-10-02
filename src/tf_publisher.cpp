dfaa88c715c5ac20e9fe8ce38e8f39a7f96e2a86
eric johnson <eric.emj.johnson@gmail.com>
2018-10-02 16:15:37 -0500

    first commit

--- src/tf_publisher.cpp
+++ src/tf_publisher.cpp
@@ -6,7 +6,8 @@
 #include <Eigen/Dense>
 #include "std_msgs/String.h"
 #include "arena_tf_publisher/transformations.hpp"
-
+Eigen::Matrix3d ecef2enu_rotMatrix(Eigen::Vector3d ECEF);
+Eigen::Vector3d ecef2enu(Eigen::Vector3d ECEF);
 
 void poseCallback(const nav_msgs::Odometry::ConstPtr& msg){
   static tf::TransformBroadcaster br;
@@ -20,10 +21,8 @@ void poseCallback(const nav_msgs::Odometry::ConstPtr& msg){
                    msg->pose.pose.orientation.z,
                    msg->pose.pose.orientation.w);
   transform.setRotation(q);
-  br.sendTransform(tf::StampedTransform(transform,
-                                        ros::Time::now(),
-                                        "base_link",  // Parent frame
-                                        msg->header.frame_id));
+  br.sendTransform(tf::StampedTransform(transform, ros::Time::now(), msg->header.frame_id, "base_link"));
+
 }
 
 int main(int argc, char** argv){
@@ -33,27 +32,29 @@ int main(int argc, char** argv){
 
   // Get arena center so we publish it into tf
   Eigen::Vector3d baseECEF_vector;
-  ros::param::get("arenaCenterX", baseECEF_vector(0));
-  ros::param::get("arenaCenterY", baseECEF_vector(1));
-  ros::param::get("arenaCenterZ", baseECEF_vector(2));
+  baseECEF_vector(0) = 0;
+  baseECEF_vector(1) = 0;
+  baseECEF_vector(2) = 0;
   Eigen::Matrix3d Recef2enu = ecef2enu_rotMatrix(baseECEF_vector);
 
   // Publish arena center
-  static tf2_ros::StaticTransformBroadcaster static_broadcaster;
-  geometry_msgs::TransformStamped static_transformStamped;
+  // static tf2_ros::StaticTransformBroadcaster static_broadcaster;
+  // geometry_msgs::TransformStamped static_transformStamped;
   // TODO: Get center of arena in ECEF frame
 
+  ros::Subscriber sub = node.subscribe<nav_msgs::Odometry>("/mavros/local_position/odom", 1, poseCallback);
+
   // Get all quad names and create a subscriber for each one of them
-  std::vector<std::string> quadNames;
-  node.getParam("QuadList", quadNames);
-  std::vector<ros::Subscriber> subs;
-  for(int i =0; i < quadNames.size(); i++)
-  {
-    std::string topic_name;
-    topic_name = "/" + quadNames[i] + "/odom";
-    ROS_INFO("[tf publisher]: Subscribing to: %s", topic_name.c_str());
-    subs.push_back(node.subscribe(topic_name, 10, &poseCallback));
-  }
+  // std::vector<std::string> quadNames;
+  // node.getParam("QuadList", quadNames);
+  // std::vector<ros::Subscriber> subs;
+  // for(int i =0; i < quadNames.size(); i++)
+  // {
+  //   std::string topic_name;
+  //   topic_name = "/" + quadNames[i] + "/odom";
+  //   ROS_INFO("[tf publisher]: Subscribing to: %s", topic_name.c_str());
+  //   subs.push_back(node.subscribe(topic_name, 10, &poseCallback));
+  // }
 
   ros::spin();
   return 0;

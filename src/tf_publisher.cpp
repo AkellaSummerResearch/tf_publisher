471c0a302abbc08e612c259f8b246ee872904a6b
Marcelino <marcelino.malmeidan@gmail.com>
2018-10-05 14:51:29 -0500

    changed package name; cleaned code

--- src/tf_publisher.cpp
+++ src/tf_publisher.cpp
@@ -5,9 +5,6 @@
 #include <tf2/LinearMath/Quaternion.h>
 #include <Eigen/Dense>
 #include "std_msgs/String.h"
-#include "arena_tf_publisher/transformations.hpp"
-Eigen::Matrix3d ecef2enu_rotMatrix(Eigen::Vector3d ECEF);
-Eigen::Vector3d ecef2enu(Eigen::Vector3d ECEF);
 
 void poseCallback(const nav_msgs::Odometry::ConstPtr& msg){
   static tf::TransformBroadcaster br;
@@ -29,18 +26,6 @@ int main(int argc, char** argv){
   ros::init(argc, argv, "arena_tf_broadcaster");
   ros::NodeHandle node("~");
   ROS_INFO("Tf broadcaster started!");
-
-  // Get arena center so we publish it into tf
-  Eigen::Vector3d baseECEF_vector;
-  baseECEF_vector(0) = 0;
-  baseECEF_vector(1) = 0;
-  baseECEF_vector(2) = 0;
-  Eigen::Matrix3d Recef2enu = ecef2enu_rotMatrix(baseECEF_vector);
-
-  // Publish arena center
-  // static tf2_ros::StaticTransformBroadcaster static_broadcaster;
-  // geometry_msgs::TransformStamped static_transformStamped;
-  // TODO: Get center of arena in ECEF frame
 
   ros::Subscriber sub = node.subscribe<nav_msgs::Odometry>("/mavros/local_position/odom", 1, poseCallback);
 
